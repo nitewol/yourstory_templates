6dabefa346dc0707105a54103cf1c15e22a6f50b
unknown <nitewol@gmail.com>
2010-01-18 04:00:28 +0000

    all templates, added print.css, needs jQ and old more old browser testing; see _to_do.txt

---
+++ js/suckerfish_ie.js
@@ -0,0 +1,23 @@
+function sfHoverEvents(sfEls) {
+		
+  var len = sfEls.length;
+  for (var i=0; i<len; i++) {
+    sfEls[i].onmouseover=function() {
+      this.className+=" sfhover";
+    }
+    sfEls[i].onmouseout=function() {
+    	
+      this.className=this.className.replace(" sfhover", "");
+    }
+  }
+}
+function sfHover() {
+	document.write("hellp");
+var ULs = document.getElementsByTagName("UL");
+var len = ULs.length;
+  for(var i=0;i<len;i++) {
+    if(ULs[i].className.indexOf("ie6helper") != -1)
+      sfHoverEvents(ULs[i].getElementsByTagName("LI"));
+  }
+}
+if (window.attachEvent) window.attachEvent("onload", sfHover);
